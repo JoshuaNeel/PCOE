e1136341ca06f4e07491bbb7844dae8e46d28c8b
sayimpuvinay <vinaysayimpu@gmail.com>
2022-11-04 16:16:12 +0530

    Added checkboxes, radio buttons to accordion

---
+++ src/common_components/filtersData.js
@@ -0,0 +1,16 @@
+export const l1_l2 = ["L1=1, L2=1","L1=1, L2=2", "L1=2, L2=1", "L1=2, L2=2"];
+export const countryOfOrigin = ["US", "Germany", "UK", "Ireland"];
+export const PMEGroup =['Crypto', 'ADR', 'LPS'] ;
+export const productGroupToInclude = ["22-US ETF", "82-Canadian ETF", "20-CommonShares", "21-canadian Stock"];
+export const productGroupToExclude = ["22-US ETF", "82-Canadian ETF", "20-CommonShares", "21-canadian Stock"];
+export const securitiesToInclude = [{name: "Tickers", ticker:"GOOG"}, 
+                                    {name: "Tickers", ticker:"GOOG"}, 
+                                    {name: "ISIN", ticker:"ABC123"},
+                                    {name: "ISIN", ticker:"ABC246"}];
+export const securitiesToExclude = [{name: "Tickers", ticker:"GOOG"}, 
+                                    {name: "Tickers", ticker:"GOOG"}, 
+                                    {name: "ISIN", ticker:"ABC123"},
+                                    {name: "ISIN", ticker:"ABC246"}];
+
+export const optionInclusion = ["Options and Equities", "Options Only", "Equities Only"];
+export const marginability = ["Both Marginable and non marginable securities", "Marginable Securities", "Non Marginable Securities"];
--- src/common_components/radioButton.js
+++ src/common_components/radioButton.js
@@ -1,50 +1,14 @@
-import { useState } from 'react';
 
-const RadioButton = (props) => {
+export const RadioButton = (props) => {
 
-    return (
-                <input style={{width: '12px', height: '12px',boxShadow: 'none', marginRight: '3px'}} id="radioButtonOptions" type="radio" value={props.value} name={props.name} onChange={props.onChange} />
-    )
-}
-
-export default RadioButton;
-
-
-const RenderRadioButton = (props) => {
-    const radiobuttons = [
-        {
-          name: 'gender',
-          key: 'Female',
-          value: 'Female',
-        },
-        {
-          name: 'gender',
-          key: 'Male',
-          value: 'Male',
-        },
-        {
-          name: 'gender',
-          key: 'Others',
-          value: 'Others',
-        },
-      ];
-
-
-    const [radioButtonValue, setRadioButtonValue] = useState();
-    const handleRadiobutton = (e) => {
-      setRadioButtonValue(e.target.value);
-    }
-
-    return (
-        <section style={{display: "grid"}}>
-          {
-          radiobuttons.map(item => (
-            <label key={item.key}>
-              <RadioButton value={item.value} name={item.name} onChange={(e) => handleRadiobutton(e)} />
-              {item.value}
-            </label>
-          ))
-        }
-        </section>
-    );
+  return (
+    <section style={{display: "grid"}}>
+      {props.value.map((radioItem, index) => (
+        <label key={index}>
+            <input style={{width: '12px', height: '12px',boxShadow: 'none', marginRight: '3px'}} id="radioButtonOptions" type="radio" value={radioItem} name={props.name} onChange={props.onChange} />
+            {radioItem}
+        </label>
+      ))}
+    </section>
+  )
 }
